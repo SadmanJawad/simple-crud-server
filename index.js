cb47b415c47650551e2bbca580ef495bcbd3c7ba
SadmanJawad <jawadnabil977@gmail.com>
2023-05-09 04:31:10 +0600

    Put And Patch And Update User To Database

--- index.js
+++ index.js
@@ -35,38 +35,58 @@ async function run() {
 
     // ! CRUD
     // get সাধারণত post এর আগে সবাই বসায়
-    
+
     // Read multiple document
-    app.get('/users', async( req, res) => {
+    app.get('/users', async (req, res) => {
       const cursor = userCollection.find()
       const result = await cursor.toArray();
       res.send(result)
     })
 
     //Load Single Item By Id 
-    app.get('/users/:id', async(req, res) => {
+    app.get('/users/:id', async (req, res) => {
       const id = req.params.id;
 
-      const query = {_id : new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const user = await userCollection.findOne(query);
       res.send(user)
     })
-    
-    // Create
-    app.post('/users', async(req, res) => {
-        const user = req.body;
-        console.log('new user', user)
 
-        const result = await userCollection.insertOne(user);
-        res.send(result);
+    // Create
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log('new user', user)
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
     });
     // Delete
-    app.delete('/users/:id', async(req, res) => {
+    app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
       console.log('Please Delete this id from database', id)
-      const query = {_id : new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query);
       res.send(result);
+    })
+
+    // put
+    app.put('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      console.log(id, user);
+      // filter=query
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updatedUser = {
+        $set: {
+          name: user.name,
+          email: user.email
+        }
+      }
+
+const result = await userCollection.updateOne(filter, updatedUser, options)
+res.send(result);
+
     })
 
 
@@ -76,7 +96,7 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } 
+  }
   finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -85,10 +105,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=> {
-    res.send('SIMPLE CRUD IS RUNNING')
+app.get('/', (req, res) => {
+  res.send('SIMPLE CRUD IS RUNNING')
 })
 
-app.listen(port, ()=> {
-    console.log(`SIMPLE CRUD is running on port, ${port}`)
+app.listen(port, () => {
+  console.log(`SIMPLE CRUD is running on port, ${port}`)
 })
